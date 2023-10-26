d82c3a17a8ef9ac8fbb77be6bd19970ab190e8be
Muckilein <JuliaWessolleck@t-online.de>
2023-10-26 20:32:34 +0200

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginScreenComponent
   ],
   imports: [
     BrowserModule,
