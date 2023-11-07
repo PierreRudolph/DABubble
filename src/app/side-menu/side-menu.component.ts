e2c64a770eccbba6cc92dc4df793826bda23e9cb
PierreRudolph <Plettner@hotmail.de>
2023-11-07 13:13:15 +0100

    commit

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -9,29 +9,29 @@ import { ChatHepler } from 'src/moduls/chatHelper.class';
   styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent {
-  menuHidden: boolean | true | undefined;
+  menuHidden: boolean | false | undefined;
   chPanelOpen: boolean | undefined;
   mesPanelOpen: boolean | undefined;
-  private chathelper : ChatHepler = new ChatHepler();
+  private chathelper: ChatHepler = new ChatHepler();
   @Input() user: User = new User();
   @Input() userList = [this.user];
-  @Input() threadList = [{"channel":{"name":"channelname"}}]// [this.chathelper.createEmptyThread()];
+  @Input() threadList = [{ "channel": { "name": "channelname" } }]// [this.chathelper.createEmptyThread()];
   @Output() newItemEventUser = new EventEmitter<User>();
   @Output() newItemEvent = new EventEmitter<boolean>();
-  public loaded:boolean=false;
+  public loaded: boolean = false;
 
   constructor(public dialog: MatDialog) {
     console.log("threadist construktor", this.threadList);
-    console.log("channel name",this.threadList[0].channel.name);
-    setTimeout(()=>{
+    console.log("channel name", this.threadList[0].channel.name);
+    setTimeout(() => {
       this.loaded = true; //wegen ladeproblemen
-    },225);
-   }
+    }, 225);
+  }
 
-  getName(num:number){
-    if(this.loaded){return  this.threadList[num].channel.name }
-    else{return ""};
-   }
+  getName(num: number) {
+    if (this.loaded) { return this.threadList[num].channel.name }
+    else { return "" };
+  }
 
   addNewItem(user: User) {
     this.newItemEventUser.emit(user);
@@ -47,6 +47,6 @@ export class SideMenuComponent {
     this.dialog.open(CreateChannelDialogComponent);
   }
 
-  openThread(){}
+  openThread() { }
 
 }
