c675b05b1ef7337807274280c0c737175a66d28b
Muckilein <JuliaWessolleck@t-online.de>
2023-11-19 12:27:28 +0100

    pop ups login

--- src/app/create-account-main/create-account-main.component.ts
+++ src/app/create-account-main/create-account-main.component.ts
@@ -8,7 +8,7 @@ import { User } from 'src/moduls/user.class';
 })
 export class CreateAccountMainComponent {
 
-  public fristpage = true;
+  public fristpage =true;
   public user: User = new User();
 
   construkter() {  
