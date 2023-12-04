f0b453337377dfe22a03dc22b908dfe9e756fb80
Muckilein <JuliaWessolleck@t-online.de>
2023-12-04 11:48:11 +0100

    date

---
+++ src/app/transform-date.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ChatHepler } from 'src/moduls/chatHelper.class';
+
+@Pipe({
+  name: 'transformDate'
+})
+export class TransformDatePipe implements PipeTransform {
+
+  private chatHelper: ChatHepler = new ChatHepler();
+
+  transform(value: string, ...args: unknown[]): unknown {
+    let date= this.chatHelper.parseDate(new Date(Date.now()));
+    if(date == value)    
+    return "Heute";
+    else return value;
+  }
+
+}
